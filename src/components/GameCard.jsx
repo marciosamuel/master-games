168f6008833386fcf4ca85eb2eb9b3403c9b0b93
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-20 19:54:42 -0300

    Adicionado card de informações dos jogos

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Card } from "../styles";
+import FilledStar from "../assets/FilledStar.svg";
+import EmptyStar from "../assets/EmptyStar.svg";
+
+function GameCard(props) {
+  const { title, platform, genre: category, thumbnail: image } = props;
+  const [rate, setRate] = useState(0);
+  return (
+    <Card>
+      <h3 className="title">{title}</h3>
+      <p className="info">
+        Plataforma: {platform}
+        <br />
+        Categoria: {category}
+      </p>
+      <img src={image} />
+      <span className="rating">
+        {[...Array(5)].map((item, index) => {
+          return (
+            <img
+              key={index}
+              src={index < rate ? FilledStar : EmptyStar}
+              onClick={() => setRate(index + 1)}
+            />
+          );
+        })}
+      </span>
+      {rate > 0 && (
+        <small
+          className="clear-rate"
+          onClick={() => {
+            setRate(0);
+          }}
+        >
+          Limpar avaliação
+        </small>
+      )}
+    </Card>
+  );
+}
+
+export default GameCard;
