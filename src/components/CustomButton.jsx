150f406abeaada79bb6de655df3ac3f097fd1888
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-20 19:54:28 -0300

    Adicionado Botão e input personalizado

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Button } from "../styles";
+
+function CustomButton(props) {
+  const { handleClick, title, disabled, size } = props;
+
+  return (
+    <Button disabled={disabled} onClick={handleClick} size={size}>
+      {title}
+    </Button>
+  );
+}
+
+export default CustomButton;
---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Input } from "../styles";
+
+function CustomInput(props) {
+  const { handleChange, value, type, label, error, errorMessage } = props;
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <Input>
+      {isFocus && <span className="placeholder">{label}</span>}
+      <input
+        onClick={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(event) => handleChange(event.target.value)}
+        value={value}
+        placeholder={isFocus ? "" : label}
+        type={type}
+      />
+      {error && <small>{errorMessage}</small>}
+    </Input>
+  );
+}
+
+export default CustomInput;
