bb65fec6aef9c28f40832ecba0d0a31e413af752
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-22 14:38:24 -0300

    Página inicial da aplicação

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,63 @@
-import React, { useState, useMemo, useEffect } from "react";
-import { ActionsContainer, GamesContainer } from "../styles";
-import CustomInput from "../components/CustomInput";
-import CustomButton from "../components/CustomButton";
-import ContentContainer from "../components/ContentContainer";
-import { getAll } from "../requests";
-import GameCard from "../components/GameCard";
+import React, { useEffect, useMemo, useState } from 'react';
+import { ActionsContainer, GamesContainer, Select } from './styles';
+import CustomInput from '../components/CustomInput';
+import ContentContainer from '../components/ContentContainer';
+import GameCard from '../components/GameCard';
+import { getAll } from '../requests';
 
 function Home() {
-  document.title = "Master Games - Página incial";
-
-  const [searchName, setSearchName] = useState("");
   const [isLoading, setIsLoading] = useState(true);
-  const [sortOption, setSortOption] = useState("A-Z");
   const [allGames, setAllGames] = useState(null);
   const [errors, setErrors] = useState(null);
+  const [searchName, setSearchName] = useState('');
+  const [sortOption, setSortOption] = useState('A-Z');
+  const [category, setCategory] = useState('Categorias');
+  const [platform, setPlatform] = useState('Plataformas');
 
   const sortedGames = useMemo(() => {
     if (allGames) {
       switch (sortOption) {
-        case "A-Z":
+        case 'A-Z':
           return allGames.sort((a, b) => (a.title <= b.title ? -1 : 1));
-        case "Z-A":
+        case 'Z-A':
           return allGames.sort((a, b) => (a.title >= b.title ? -1 : 1));
-        case "DATE (desc)":
+        case 'DATE (desc)':
           return allGames.sort((a, b) =>
             a.release_date <= b.release_date ? -1 : 1
           );
-        case "DATE (asc)":
+        case 'DATE (asc)':
           return allGames.sort((a, b) =>
             a.release_date >= b.release_date ? -1 : 1
           );
         default:
-          allGames;
+          return allGames;
       }
     }
     return [];
   }, [allGames, sortOption]);
 
   const matchedGames = useMemo(() => {
+    if (searchName.length >= 3 && category !== 'Não selecionado') {
+      return sortedGames
+        .filter((game) =>
+          game.title.toLowerCase().includes(searchName.toLowerCase())
+        )
+        .filter((game) => game.genre === category);
+    }
     if (searchName.length >= 3)
-      return sortedGames.filter((game) => {
-        return game.title.toLowerCase().includes(searchName.toLowerCase());
-      });
+      return sortedGames.filter((game) =>
+        game.title.toLowerCase().includes(searchName.toLowerCase())
+      );
+    if (category !== 'Não selecionado') {
+      return sortedGames.filter((game) => game.genre === category);
+    }
     return sortedGames;
-  }, [sortedGames, searchName]);
+  }, [sortedGames, searchName, category, platform]);
 
   const categories = useMemo(() => {
     const categoryList = [];
-    if (matchedGames) {
-      matchedGames.forEach((game) => {
+    if (allGames) {
+      allGames.forEach((game) => {
         const { genre } = game;
         if (!categoryList.includes(genre)) {
           categoryList.push(genre);
@@ -57,12 +66,12 @@ function Home() {
     }
     console.warn(categoryList);
     return categoryList;
-  }, [matchedGames]);
+  }, [allGames]);
 
   const platforms = useMemo(() => {
     const platformList = [];
-    if (matchedGames) {
-      matchedGames.forEach((game) => {
+    if (allGames) {
+      allGames.forEach((game) => {
         const { platform } = game;
         if (!platformList.includes(platform)) {
           platformList.push(platform);
@@ -71,7 +80,7 @@ function Home() {
     }
     console.warn(platformList);
     return platformList;
-  }, [matchedGames]);
+  }, [allGames]);
 
   const getGames = async () => {
     await getAll()
@@ -94,31 +103,36 @@ function Home() {
   return (
     <ContentContainer actions>
       <ActionsContainer>
-        <div className="search-game">
-          <CustomInput
-            label="Busque por nome"
-            value={searchName}
-            handleChange={setSearchName}
-            type="text"
-            error={searchName.length < 3 && searchName !== ""}
-            errorMessage="Você precisa digitar pelo menos 3 caracteres"
-          />
-          <CustomButton
-            title="Buscar"
-            size="small"
-            handleClick={() => console.log("hello!")}
-            disabled={searchName.length < 3 || searchName === ""}
-          />
-        </div>
+        <CustomInput
+          label="Busque por nome"
+          value={searchName}
+          handleChange={setSearchName}
+          type="text"
+          error={searchName.length < 3 && searchName !== ''}
+          errorMessage="Você precisa digitar pelo menos 3 caracteres"
+        />
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="Não selecionado" hidden>
+            Filtrar por categoria
+          </option>
+          {categories.map((cat) => (
+            <option value={cat} key={cat}>
+              {cat}
+            </option>
+          ))}
+        </Select>
       </ActionsContainer>
 
       {isLoading && <h3>Carregando...</h3>}
       {errors && <h3>Deu erro</h3>}
       {matchedGames && (
         <GamesContainer>
-          {matchedGames.map((game) => {
-            return <GameCard key={game.id} {...game} />;
-          })}
+          {matchedGames.map((game) => (
+            <GameCard key={game.id} {...game} />
+          ))}
         </GamesContainer>
       )}
     </ContentContainer>
