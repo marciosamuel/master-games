774ae66ec6a88910bd73495592c03b0d4e9421f7
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-22 14:41:05 -0300

    Adicionado configurações e libs de linter e prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
-export default {
+module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: ['airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'react/prop-types': [0],
+    'react/jsx-props-no-spreading': [0],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
 };
