cdc65cf13313e89aa11dcef5d08a136196ae80dc
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-22 21:09:06 -0300

    Finalização da página de Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,89 +1,20 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { ActionsContainer, GamesContainer, Select } from './styles';
-import CustomInput from '../components/CustomInput';
-import ContentContainer from '../components/ContentContainer';
-import GameCard from '../components/GameCard';
-import { getAll } from '../requests';
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import { ContentWrapper } from '../components/styles';
+import GamesList from '../components/GamesList';
+import Filters from '../components/Filters';
+import getGames from '../requests';
 
-function Home() {
+export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const [allGames, setAllGames] = useState(null);
   const [errors, setErrors] = useState(null);
-  const [searchName, setSearchName] = useState('');
-  const [sortOption, setSortOption] = useState('A-Z');
-  const [category, setCategory] = useState('Categorias');
-  const [platform, setPlatform] = useState('Plataformas');
+  const [sortedGames, setSortedGames] = useState(null);
+  const [filters, setFilters] = useState({});
 
-  const sortedGames = useMemo(() => {
-    if (allGames) {
-      switch (sortOption) {
-        case 'A-Z':
-          return allGames.sort((a, b) => (a.title <= b.title ? -1 : 1));
-        case 'Z-A':
-          return allGames.sort((a, b) => (a.title >= b.title ? -1 : 1));
-        case 'DATE (desc)':
-          return allGames.sort((a, b) =>
-            a.release_date <= b.release_date ? -1 : 1
-          );
-        case 'DATE (asc)':
-          return allGames.sort((a, b) =>
-            a.release_date >= b.release_date ? -1 : 1
-          );
-        default:
-          return allGames;
-      }
-    }
-    return [];
-  }, [allGames, sortOption]);
-
-  const matchedGames = useMemo(() => {
-    if (searchName.length >= 3 && category !== 'Não selecionado') {
-      return sortedGames
-        .filter((game) =>
-          game.title.toLowerCase().includes(searchName.toLowerCase())
-        )
-        .filter((game) => game.genre === category);
-    }
-    if (searchName.length >= 3)
-      return sortedGames.filter((game) =>
-        game.title.toLowerCase().includes(searchName.toLowerCase())
-      );
-    if (category !== 'Não selecionado') {
-      return sortedGames.filter((game) => game.genre === category);
-    }
-    return sortedGames;
-  }, [sortedGames, searchName, category, platform]);
-
-  const categories = useMemo(() => {
-    const categoryList = [];
-    if (allGames) {
-      allGames.forEach((game) => {
-        const { genre } = game;
-        if (!categoryList.includes(genre)) {
-          categoryList.push(genre);
-        }
-      });
-    }
-    console.warn(categoryList);
-    return categoryList;
-  }, [allGames]);
-
-  const platforms = useMemo(() => {
-    const platformList = [];
-    if (allGames) {
-      allGames.forEach((game) => {
-        const { platform } = game;
-        if (!platformList.includes(platform)) {
-          platformList.push(platform);
-        }
-      });
-    }
-    console.warn(platformList);
-    return platformList;
-  }, [allGames]);
-
-  const getGames = async () => {
-    await getAll()
+  const getAllGames = async () => {
+    setIsLoading(true);
+    await getGames(filters)
       .then((r) => {
         const games = r.data;
         setAllGames(games);
@@ -97,46 +28,32 @@ function Home() {
   };
 
   useEffect(() => {
-    getGames();
-  }, []);
+    getAllGames();
+  }, [filters]);
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <h1>Carregando...</h1>;
+    }
+    if (errors) {
+      return <h1>Erro!!!</h1>;
+    }
+    if (allGames) {
+      return <GamesList values={sortedGames} />;
+    }
+  };
 
   return (
-    <ContentContainer actions>
-      <ActionsContainer>
-        <CustomInput
-          label="Busque por nome"
-          value={searchName}
-          handleChange={setSearchName}
-          type="text"
-          error={searchName.length < 3 && searchName !== ''}
-          errorMessage="Você precisa digitar pelo menos 3 caracteres"
+    <>
+      <Header path="home" />
+      <ContentWrapper>
+        {renderContent()}
+        <Filters
+          values={allGames}
+          handleChange={setSortedGames}
+          setFilters={setFilters}
         />
-        <Select
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        >
-          <option value="Não selecionado" hidden>
-            Filtrar por categoria
-          </option>
-          {categories.map((cat) => (
-            <option value={cat} key={cat}>
-              {cat}
-            </option>
-          ))}
-        </Select>
-      </ActionsContainer>
-
-      {isLoading && <h3>Carregando...</h3>}
-      {errors && <h3>Deu erro</h3>}
-      {matchedGames && (
-        <GamesContainer>
-          {matchedGames.map((game) => (
-            <GameCard key={game.id} {...game} />
-          ))}
-        </GamesContainer>
-      )}
-    </ContentContainer>
+      </ContentWrapper>
+    </>
   );
 }
-
-export default Home;
