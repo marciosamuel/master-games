decdb2c0d973eebf9997ea53150878f85049d790
Marcio Samuel <marciosamuel12@outlook.com>
2021-08-22 21:09:34 -0300

    Remoção de estilos desnecessários e novo wrapper de conteúdo

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,51 +1,10 @@
 import styled from 'styled-components';
 
-export const ActionsContainer = styled.nav`
-  width: 90%;
-  margin: 1rem auto;
-  height: 75px;
+export const ContentWrapper = styled.main`
+  width: 100%;
+  min-height: calc(100vh - 51px);
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-  }
-
-  .search-game {
-    width: auto;
-    display: flex;
-  }
+  padding: 1rem;
 `;
 
-export const GamesContainer = styled.main`
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  justify-content: center;
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-  }
-`;
-
-export const Select = styled.select`
-  height: 2.5rem;
-  margin: auto 0;
-  padding: 0 1rem;
-  border-radius: 20px;
-  border-style: solid;
-  border-width: 2px;
-  border-image: linear-gradient(45deg, var(--cyan), var(--blue)) 1;
-  outline: none;
-  cursor: pointer;
-  background-color: var(--background);
-  color: var(--secondary-text);
-  font-size: 1rem;
-
-  option {
-    color: var(--secondary-text);
-  }
-`;
+export const teste = [];
